08736c8f4c8b9b93e71eee702614af2f90610a74
jholm9270 <jacobholm9270@yahoo.com>
2017-02-22 15:09:18 -0600

    A bunch of laravel install stuff. Addedc stuff to Tom's file

--- DnDCharacterBuilder/app/routes.php
+++ DnDCharacterBuilder/app/routes.php
@@ -11,7 +11,13 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('/', function(){
+    return view('welcome');
 });
+Route::get('DnDBuilder', function()
+{
+	return view('DnDBuilder');
+});
+/*Route::get('foo', function () {
+    return 'Hello World';
+*/
